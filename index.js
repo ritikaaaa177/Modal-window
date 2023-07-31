b5207d259c5dbe008446ba10ad0001ede1618c72
Ritika Kumar <kumarritika666@gmail.com>
2023-08-01 01:03:05 +1400

    escape addition

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// const btnall = document.querySelector(".show-model");
-// console.log(btnall.textContent);
+const btnall = document.querySelector(".show-model");
+console.log(btnall.textContent);
 
 const btnallopen = document.querySelectorAll(".show-model");
 console.log(btnallopen);
@@ -11,9 +11,9 @@ const model = document.querySelector(".model");
 const overlay = document.querySelector(".overlay");
 
 const opendiv = function (event) {
-  const buttonevent = event.target.dataset.text;
-  const modelText = document.querySelector("p");
-  modelText.textContent = buttonevent;
+  // const buttonevent = event.target.dataset.text;
+  // const modelText = document.querySelector("p");
+  // modelText.textContent = buttonevent;
   model.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -25,10 +25,25 @@ const closediv = function () {
 
 for (let i = 0; i < btnallopen.length; i++) {
   btnallopen[i].addEventListener("click", opendiv);
-  btnallclose.addEventListener("click", closediv);
-  overlay.addEventListener("click", closediv);
 }
 
+overlay.addEventListener("click", closediv);
+
+btnallclose.addEventListener("click", closediv);
 // document.getElementById("#model1").addEventListener("click", function () {
 //   model.style.display = "block";
 // });
+
+// document.addEventListener("keypress", function () {
+//   console.log("The key is pressed");
+// });
+// document.addEventListener("keydown", function () {
+//   console.log("The key is down");
+// });
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !model.classList.contains("hidden")) {
+    closediv();
+  }
+});
